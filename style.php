5d829994cf22b54e322cfec8f108dd52f9726710
Russ0Brook986 <adrian.pajak.marston987@gmail.com>
2025-11-28 15:19:47 +0100

    Create style.php

---
+++ style.php
@@ -0,0 +1,22 @@
+table, td , tr{
+    border: 1px solid black;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    font-size: 15px;
+    font-weight: bold;    
+}
+table{
+    width: 1900px;
+    height: 250px;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    margin-bottom: 50px;
+}
+h1{
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    text-align: center ;
+}
+th{
+    color: darkblue;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    font-size: 20px;
+    font-weight: bold;
+}
